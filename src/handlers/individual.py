42dd6297de7798eda21430087ddea70a8ea3ae36
Валерия <laifhaktv345@gmail.com>
2026-03-23 00:27:46 +0300

    feat: реализовала индивидуальный режим

---
+++ src/handlers/individual.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(lambda m: m.text == "Я сам за себя")
+async def individual_mode(message: Message):
+    await message.answer(
+        "✅ Супер! Ты в индивидуальном режиме"
+    )

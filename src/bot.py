d72c85a3177a532ee1c6289f2d403b0a83738b5f
Валерия <laifhaktv345@gmail.com>
2026-03-24 01:51:47 +0300

    fix: разрешение конфликта в 2.6

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,9 @@ import os
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 from handlers.start import router as start_router
-from handlers.individual import router as individual_router
+from handlers.tasks import router as tasks_router
+from handlers.registration import router as registration_router
+from handlers.join_group import router as join_group_router
 
 load_dotenv()
 
@@ -12,7 +14,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 dp.include_router(start_router)
-dp.include_router(individual_router)
+dp.include_router(tasks_router)
+dp.include_router(registration_router)
+dp.include_router(join_group_router)
 
 async def main():
     await dp.start_polling(bot)

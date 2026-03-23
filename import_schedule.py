f3db620bcf18476d6a065224cc44299bba79906c
alice <alisazaharcuk629@gmail.com>
2026-03-23 22:54:15 +0300

    Добавлен файл import_schedule.py

---
+++ import_schedule.py
@@ -0,0 +1,53 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.filters import Command
+
+Keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="📊Загрузить Excel-файл(рекомендую)")],
+                                          [KeyboardButton(text="📸Отправить фото(распознаю текст)")],
+                                          [KeyboardButton(text="✍️Ввести вручную")]],
+                               resize_keyboard=True,
+                               one_time_keyboard=True
+                               )
+
+bot = Bot(token="НАШ ТОКЕН Я НЕ ЗНАЮ")
+dp = Dispatcher()
+
+
+@dp.message(Command("import_schedule"))
+async def answer(message: Message):
+    await message.answer("Каким способом хочешь импортировать расписание?",
+                         reply_markup=Keyboard)
+
+
+@dp.message(F.text == "📊Загрузить Excel-файл(рекомендую)")
+async def handle_excel(message: Message):
+    await message.answer("Ты выбрал Excel-файл. Жду твоего расписания!",
+                         reply_markup=ReplyKeyboardRemove()
+                         )
+
+
+@dp.message(F.text == "📸Отправить фото(распознаю текст)")
+async def handle_excel(message: Message):
+    await message.answer("Ты выбрал отправить фото. Жду твоего расписания!",
+                         reply_markup=ReplyKeyboardRemove()
+                         )
+
+
+@dp.message(F.text == "✍️Ввести вручную")
+async def handle_excel(message: Message):
+    await message.answer("Ты выбрал ввести вручную. Жду твоего расписания!",
+                         reply_markup=ReplyKeyboardRemove()
+                         )
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот выключен")
+    asyncio.run(main())

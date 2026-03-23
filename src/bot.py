dd8a07fb4109875271e74e83a3af8e89b748a248
Валерия <laifhaktv345@gmail.com>
2026-03-24 00:51:21 +0300

    feat: реализовала вступление в группу(для участника) и исправила конфликты

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 from handlers.start import router as start_router
 from handlers.registration import router as registration_router
+from handlers.join_group import router as join_group_router
 
 load_dotenv()
 
@@ -13,6 +14,7 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 dp.include_router(start_router)
 dp.include_router(registration_router)
+dp.include_router(join_group_router)
 
 async def main():
     await dp.start_polling(bot)
--- src/handlers/start.py
+++ src/handlers/start.py
@@ -12,11 +12,3 @@ async def cmd_start(message: Message):
         [KeyboardButton(text="Я сам за себя")]
     ], resize_keyboard=True)
     await message.answer("Sup! Ты попал NAKOL💫 Кто ты?", reply_markup=kb)
-
-@router.message(lambda m: m.text == "Я в группе")
-async def role_member(message: Message):
-    await message.answer("Ты участник группы! Введи код группы 🔑")
-
-@router.message(lambda m: m.text == "Я сам за себя")
-async def role_individual(message: Message):
-    await message.answer("Индивидуальный режим! Работаешь самостоятельно 💪")
